a8e009199c133131e8111f66a0341641188c89a0
Jerry Zhang <jerryzh168@gmail.com>
2020-06-09 09:56:36 -0700

    [quant][graphmode] Fix a corner case in handling `if` in insert_observers
    Summary:
    corner case: https://github.com/pytorch/vision/blob/master/torchvision/models/densenet.py#L87
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 0d8e2377294414d2fad92c4aba32b43f9e54e893
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39615

--- test/quantization/test_quantize_script.py
+++ test/quantization/test_quantize_script.py
@@ -751,7 +751,61 @@ graph(%input, %weight):
             else:
                 m = torch.jit.script(M())
             m = prepare_script(m.eval(), {'': default_qconfig})
-            assert len(attrs_with_prefix(m, '_observer_',)) == 3
+            assert len(attrs_with_prefix(m, '_observer_')) == 3
+
+    def test_insert_observers_for_if_consistent_observation(self):
+        """ check quantization for if works as long as
+        output of all branches are quantized/observed consistently
+        """
+        class M(torch.nn.Module):
+            def __init__(self, cond):
+                super(M, self).__init__()
+                self.conv = torch.nn.Conv2d(3, 3, 3).float()
+                self.cond = cond
+
+            def forward(self, x):
+                x = self.conv(x)
+                # x is already observed
+                if self.cond:
+                    x = torch.flatten(x)
+                return x
+
+        class M2(torch.nn.Module):
+            def __init__(self, cond):
+                super(M2, self).__init__()
+                self.conv1 = torch.nn.Conv2d(3, 3, 3).float()
+                self.conv2 = torch.nn.Conv2d(3, 3, 3).float()
+                self.cond = cond
+
+            def forward(self, x):
+                x = self.conv1(x)
+                if self.cond:
+                    x = self.conv2(x)
+                    # x will be observed in the branch
+                else:
+                    x = torch.flatten(x)
+                # since output for both branch are quantized
+                # the if node is quantized consistently
+                return x
+
+        data = torch.rand((1, 3, 5, 5), dtype=torch.float)
+        options = list(itertools.product([True, False], [True, False]))
+        for cond, tracing in options:
+            if tracing:
+                m = torch.jit.trace(M(cond), data)
+            else:
+                m = torch.jit.script(M(cond))
+            m = prepare_script(m, {'': default_qconfig})
+            assert len(attrs_with_prefix(m, '_observer_')) == 2
+
+        for cond, tracing in options:
+            if tracing:
+                m = torch.jit.trace(M2(cond), data)
+            else:
+                m = torch.jit.script(M2(cond))
+            m = prepare_script(m, {'': default_qconfig})
+            num_observers = 2 if tracing and not cond else 3
+            assert len(attrs_with_prefix(m, '_observer_')) == num_observers
 
     def test_insert_quant_dequant(self):
         class M(torch.nn.Module):
